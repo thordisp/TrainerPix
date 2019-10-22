dd962fa9116bae4d41247138967d8359a56e6b9d
Þórdís Pétursdóttir <thp44@hi.is>
2019-10-22 14:36:24 +0000

    Testing form for program, user input updates this.state

--- src/routes/newProgram/NewProgram.js
+++ src/routes/newProgram/NewProgram.js
@@ -3,10 +3,58 @@ import './NewProgram.scss';
 import Webcam from '../../components/webcam/Webcam';
 
 class NewProgram extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      client: 0,
+      sets: 0,
+      reps: 0,
+      description: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('client: ' + this.state.client + ', sets: ' + this.state.sets + ', reps: ' +
+                  this.state.reps + ', description: ' + this.state.description);
+  }
+
   render() {
     return (
-      <div className="booth">
-        <Webcam />
+      <div className="programContainer">
+        <div className="programForm">
+        <form action="/action_page.php" method="post" onSubmit={this.handleSubmit}>
+          <label>
+            Client:
+            <input type="text" name="client"  onChange={this.handleChange} />
+          </label>
+          <label>
+            Sets:
+            <input type="text" name="sets" onChange={this.handleChange} />
+          </label>
+          <label>
+            Reps:
+            <input type="text" name="reps" onChange={this.handleChange} />
+          </label>
+          <label>
+            Description:
+          <input type="text" name="description" value={this.state.description} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+        </div>
+        <div className="booth">
+          <Webcam />
+        </div>
       </div>
     )
   }
