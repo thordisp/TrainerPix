7a599e5d7931be400f803df195c2b00537bf39c5
Þórdís Pétursdóttir <thp44@hi.is>
2019-12-02 20:07:25 +0000

    Laga útlit á forsíðu

--- src/routes/home/Home.js
+++ src/routes/home/Home.js
@@ -1,11 +1,87 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/styles';
+import Paper from '@material-ui/core/Paper';
+import { Context } from '../../UserContext';
+import Login from '../login/Login';
+import ProgramForm from '../../components/programForm/ProgramForm';
 
 import './Home.scss';
 
+const styles = () => ({
+  root: {
+    padding: '20px',
+    width: '40%',
+  },
+  login: {
+    position: 'absolute',
+  }
+});
+
 class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.updateClients = this.updateClients.bind(this);
+    this.updateClientId = this.updateClientId.bind(this);
+
+    this.state = {
+      clients: [],
+      clientId: 0
+    };
+  }
+
+  updateClients(id, username) {
+    this.setState(state => {
+      const clients = state.clients.concat({ id: id, username: username });
+      return {
+        clients,
+      };
+    });
+  }
+
+  updateClientId(id) {
+    this.setState({ clientId: id });
+  }
+
   render() {
-    return <h1></h1>
+    const { classes } = this.props;
+    return (
+
+      <Context.Consumer>
+      {({ authenticated }) => {
+        return (
+          <div>
+            {authenticated && (
+              <div className="home-background">
+                <Paper className={classes.root}>
+                  <ProgramForm
+                    clients = {this.state.clients}
+                    updateClients={this.updateClients}
+                    updateClientId={this.updateClientId}
+                  />
+                </Paper>
+              </div>
+            )}
+
+            {!authenticated && (
+              <div className="home-background">
+                <Paper className={classes.root}>
+                  <Login className={classes.login}/>
+                </Paper>
+              </div>
+            )}
+          </div>
+        )
+      }}
+    </Context.Consumer>
+    )
   }
 }
 
-export default Home
+Home.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Home);
